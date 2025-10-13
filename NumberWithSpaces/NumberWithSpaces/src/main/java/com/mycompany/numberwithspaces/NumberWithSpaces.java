02fe4cdefea50ddcb50f7fb79dfc7f7233056037
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-10-13 14:56:19 +0800

    NWS fix
    Fixed code of NWS to no longer use strings and characters, now uses modulo and arrayLists.

--- NumberWithSpaces/NumberWithSpaces/src/main/java/com/mycompany/numberwithspaces/NumberWithSpaces.java
+++ NumberWithSpaces/NumberWithSpaces/src/main/java/com/mycompany/numberwithspaces/NumberWithSpaces.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.numberwithspaces;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -13,23 +14,37 @@ import java.util.Scanner;
 public class NumberWithSpaces {
 
     public static void main(String[] args) {
-        int inputNum = 0;
-        String unseparated, separated;
+        int inputNum = 0, size = 0;
+        ArrayList<Integer> uncorrected = new ArrayList<Integer>();
+        ArrayList<Integer> corrected = new ArrayList<Integer>();
+        String separated;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string of 5 digits, and this program will");
-        System.out.println(" output them separated by a space.");
+        System.out.println("Enter a string of 5 digits, and this program will"
+                + " output them separated by a space.");
         
         System.out.print("Input here: ");
         inputNum = sc.nextInt();
-        unseparated = Integer.toString(inputNum);
         
-        char a = unseparated.charAt(0);
-        char b = unseparated.charAt(1);
-        char c = unseparated.charAt(2);
-        char d = unseparated.charAt(3);
-        char e = unseparated.charAt(4);
+        while(inputNum > 0) {
+            uncorrected.add(inputNum % 10);
+            inputNum = inputNum/10;
+            size++;
+        }
         
-        separated = a+" "+b+" "+c+" "+d+" "+e;
+        for(int num : uncorrected) {
+            corrected.addFirst(num);
+        }
+        
+        separated = "";
+        int i = 0;
+        for(int sepNum : corrected) {
+            i++;
+            separated += sepNum;
+            if (i < size) {
+                separated += " ";
+            }
+        }
+        
         System.out.println(separated);
     }
 }
